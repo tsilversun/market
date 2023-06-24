e10e232f9ed261d4f0deda42c1025576556b20f8
tsilversun <34732393+tsilversun@users.noreply.github.com>
2023-06-24 11:56:17 +0800

    first

---
+++ resources/views/component/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>AJJ MARKETPLACE</title>
+</head>
+<body>
+    <div class="container px-28 py-5">
+        <nav class="flex items-center">
+            <ul class="flex-1 text-center">
+                <li class="list-none inline-block px-5"><a href="/market/home" class="no-underline">Home</a></li>
+                <li class="list-none inline-block px-5"><a href="/market/product" class="no-underline">Product</a></li>
+                <li class="list-none inline-block px-5"><a href="/market/about" class="no-underline">About</a></li>
+                <li class="list-none inline-block px-5"><a href="/market/login" class="no-underline">Login</a></li>
+                <li class="list-none inline-block px-5"><a href="/market/register" class="no-underline">Register</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
